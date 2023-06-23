cfbdad565f354bb05e32848cecce69aed7533bdf
Sharath083 <sharath.r@marketsimplified.com>
2023-06-23 11:32:11 +0530

    new

---
+++ KtorTraning/src/main/kotlin/com/example/routes/CustomerRoutes.kt
@@ -0,0 +1,77 @@
+package com.example.routes
+
+import com.example.model.Customer
+import com.example.model.customerStorage
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.ratelimit.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.io.File
+
+
+fun Route.customerRouting(){
+    route("/customer"){
+        rateLimit {
+            get {
+                if (customerStorage.isNotEmpty()) {
+                    call.respond(customerStorage)
+                } else {
+                    call.respondText("No customers found", status = HttpStatusCode.OK)
+                }
+
+            }
+        }
+        get("/download"){//we can get image through
+            val file = File("C:\\Users\\sharathkumar.b\\Pictures\\Screenshots\\PostManScreenShots/Screenshot (22).png")
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment.withParameter(ContentDisposition.Parameters.FileName, "ktorTraining.png")
+                    .toString())
+            call.response.status(HttpStatusCode(22,"Pic downloaded"))
+//            call.respondRedirect("{id?}", permanent = true) TO MOVE TO OTHER PAGE
+
+            call.respondFile(file)
+
+
+
+        }
+        get("{id?}") {
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Missing id",
+                status = HttpStatusCode.BadRequest
+            )
+            val customer =
+                customerStorage.find { it.id == id } ?: return@get call.respondText(
+                    "No customer with id $id",
+                    status = HttpStatusCode.NotFound
+                )
+            call.respond(customer)
+            call.respondText("Fetched Data")
+        }
+        post {
+            val customer=call.receive<Customer>()
+            if ((customerStorage.any { it.id==customer.id })){
+
+                call.respondText("Customer with id ${customer.id} already exists",status = HttpStatusCode.BadRequest)
+            }
+            else{
+                customerStorage.add(customer)
+                call.respondText("Customer stored correctly",status = HttpStatusCode.Created)
+            }
+        }
+        delete("{id?}") {
+            val id =call.parameters["id"]?:return@delete call.respondText("Missing id", status = HttpStatusCode.BadRequest)
+            val customer= customerStorage.removeIf { it.id==id }
+            if(customer){
+                call.respondText("Customer removed correctly", status = HttpStatusCode.Accepted)
+            }
+            else{
+                call.respondText("Customer with $id is not found", status = HttpStatusCode.NotFound)
+            }
+
+
+        }
+    }
+}
---
+++ KtorTraning/src/main/kotlin/com/example/routes/OrderRoutes.kt
@@ -0,0 +1,40 @@
+package com.example.routes
+
+import com.example.model.orderStorage
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.listOrders(){
+    get("/orders"){
+        if(orderStorage.isNotEmpty()){
+            call.respond(orderStorage)
+        }
+        else{
+            call.respondText("Empty OrdersList")
+        }
+    }
+}
+fun Route.totalizeOrderRoute() {
+    get("/order/total/{id?}") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        val total = order.contents.sumOf { it.price * it.amount }
+        call.respond("$$total")
+    }
+}
+fun Route.getOrderRoute() {
+    get("/order/{id?}") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        call.respond(order)
+
+    }
+}
